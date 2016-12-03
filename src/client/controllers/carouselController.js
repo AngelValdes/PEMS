cef2ecce099ca41604f6f91f0f1fb803b61c8d70
Angel Valdes <Angel Valdes>
2016-12-03 13:40:23 -0500

    carousel and student grid

--- src/client/controllers/carouselController.js
+++ src/client/controllers/carouselController.js
@@ -7,9 +7,9 @@
         vm.noWrapSlides = false;
         vm.active = 1;
         vm.slides = [
-            {id: 0, image: "/contents/students0.png", text: "Slide #1"},
-            {id: 1, image: "/contents/students1.png", text: "Slide #2"},
-            {id: 2, image: "/contents/students2.png", text: "Slide #3"}
+            {id: 0, image: "/contents/students0.png", imageSmall: "/contents/students0Small.png", text: "Slide #1", alt: "Students Group 1"},
+            {id: 1, image: "/contents/students1.png", imageSmall: "/contents/students1Small.png", text: "Slide #2", alt: "Students Group 2"},
+            {id: 2, image: "/contents/students2.png", imageSmall: "/contents/students2Small.png", text: "Slide #3", alt: "Students Group 3"}
         ];
     }
  })();
