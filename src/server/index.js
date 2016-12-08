bd1e8007d87643e243f06dc4c6258006aba5e94f
Angel Valdes <Angel Valdes>
2016-12-07 21:02:52 -0500

    enable cors and error handler

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,22 @@
-require('dotenv').config();
-const winston = require('winston');
-winston.add(winston.transports.File, { filename: "server.log" });
+require('dotenv').config(); //read .env variables
+const winston = require('winston'); //logging
+winston.add(winston.transports.File, { filename: "server.log" }); //logging to console & file
 const express = require("express");
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken"); //token authentication & authorazation
 const app = express();
+const cors = require('cors');//support CORS (Cross Origin)
 const port = process.env.ENV_PORT || 5000;
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); //read body from post
+app.use(function(err,req,res,next){ // Error handler
+  winston.log("error", err.message);
+  res.status(err.status || 500).json(err.message); //change to display error page
+});
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(__dirname + "/../client"));
+app.use(express.static(__dirname + "/../client")); // allow static content
 app.use(express.static(__dirname + "/../../node_modules"));
-app.use("/", require("./routes")(express, jwt));
+app.use("/", require("./routes")(express, jwt)); //add routes
 
-app.listen(port, ()=>{winston.log('info', "server running on port " + port);});
+app.listen(port, ()=>{winston.log("info", "server running on port " + port);});
 module.exports = app;
